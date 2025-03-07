847834ace7b07cfc9e5e43ff47872809f9992ea5
MartyanovaK <132018032+MartyanovaK@users.noreply.github.com>
2025-03-07 09:50:19 +0300

    Update User.java

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -32,7 +32,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
